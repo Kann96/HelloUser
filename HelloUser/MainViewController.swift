ffe689d29006eb8d6b63541ae14cfa80ce1e4494
Kann96 <kann9620@gmail.com>
2025-01-19 22:18:33 +0300

    comm1901

--- HelloUser/MainViewController.swift
+++ HelloUser/MainViewController.swift
@@ -7,13 +7,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class MainViewController: UIViewController {
 
+    
+    @IBOutlet var userName: UITextField!
+    @IBOutlet var password: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func loginHint() {
+        showAlert(withTitle: "Oops", andMessage: "Your login is User 😉")
+    }
+    @IBAction func passwordHint() {
+        showAlert(withTitle: "Oops", andMessage: "Your password is Password 😉")
+    }
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        guard userName.text == "User", password.text == "Password" else {
+            showAlert(withTitle: "Invalid login or password", andMessage: "Please, enter correct login and password")
+            return false
+        }
+        // Введенное имя валидно, разрешаем переход
+        return true
+    }
+    
+    @IBAction func logInButton() {
+    }
 }
 
+// MARK: - AlertController
+
+extension MainViewController {
+    private func showAlert(withTitle title: String, andMessage message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
